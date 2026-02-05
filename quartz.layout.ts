6751a46fdd68687b32f1d23b831b9a13a92c5c20
markjconnor <123944608+markjconnor@users.noreply.github.com>
2026-02-05 14:21:11 +0000

    Add breadcrumbs formatting

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,11 +32,16 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
+      component: Component.Breadcrumbs({
+        spacerSymbol: "/", // symbol between crumbs
+        rootName: "Home", // name of first/root element
+        resolveFrontmatterTitle: true, // whether to resolve folder names through frontmatter titles
+        showCurrentPage: true, // whether to display the current page in the breadcrumbs
+    }),
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    //Component.ContentMeta(),
     Component.TagList(),
     Component.Flex({
       components: [
@@ -52,7 +57,7 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     //Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()), 
-    Component.Explorer(),
+    //Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -60,13 +65,32 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Backlinks(),
   ],
 }
+  
+/*
+ // components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.ArticleTitle(),
+    Component.Byline(
+      Component.Flex({
+        components: [{ Component: Component.TagList(), grow: true, align: "start" }],
+        direction: "column",
+        gap: "0.5rem",
+      }),
+      Component.ContentMeta(),
+    ),
+  ],
+  left: [Component.DesktopOnly(Component.TableOfContents())],
+  right: [],
+}
+  */
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
+  beforeBody: [
+    Component.Breadcrumbs(), 
+    Component.ArticleTitle(), 
+    Component.ContentMeta(),
     Component.Flex({
       components: [
         {
@@ -76,7 +100,10 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+  ],
+  left: [
+    Component.MobileOnly(Component.Spacer()),
+    //Component.Explorer(),
   ],
   right: [],
 }
